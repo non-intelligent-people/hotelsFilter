71ad1746c3a7bbb0f9f488c588630ef690385e2e
poetaViolento <hamzaloler@gmail.com>
2019-11-11 22:00:15 +0100

    Arreglar filter
    cuidado no funciona el filter bien

--- src/app/core/model/hotel.ts
+++ src/app/core/model/hotel.ts
@@ -6,13 +6,13 @@ export class Hotel {
         private _nombre: string;
         private _categoria: number;
         private _tiposHabitacion: Habitacion[];
-
+        
         constructor(nombre: string, categoria: number, tiposHabitacion: Habitacion[]) {
                 this._nombre = nombre;
                 this._categoria = categoria;
                 this._tiposHabitacion = tiposHabitacion;
         }
-
+        
 
         public get nombre(): string {
                 return this._nombre;
@@ -25,5 +25,8 @@ export class Hotel {
         public get tiposHabitacion(): Habitacion[] {
                 return this._tiposHabitacion;
         }
-
+        
+        public set tiposHabitacion(value: Habitacion[]) {
+                this._tiposHabitacion = value;
+        }
 }
