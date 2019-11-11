cd4efcef8e9ccd20adb648571731d0cc63ba07cb
poetaViolento <hamzaloler@gmail.com>
2019-11-11 20:31:10 +0100

    f
    actualizacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,6 +25,27 @@ export class HomePage {
     // Utilizo el router
     this.router.navigate(['uno'], navigationExtras);
   }
+  getHotelCreado(){
+    let nombreBox=document.getElementById("selectNombreHabitacion");
+    let camaBox=document.getElementById("selectCama");
+    let categoriaBox=document.getElementById("selectCategoria");
+    let precio=document.getElementById("precioInput").value;
+    let nombre=nombreBox.options[nombreBox.selectedIndex].text;
+    let cama=camaBox.options[camaBox.selectedIndex].text;
+    let categoria=categoriaBox.selectedIndex;
+    let minibarBox=document.getElementById("minibar");
+    let secadorBox=document.getElementById("secador");
+    let minibar=this.getValueChecked(minibarBox);
+    let secador=this.getValueChecked(secadorBox);
+    console.log(precio,cama,nombre,categoria,minibar,secador);
+
+  }
+  getValueChecked(elemento){
+    if (elemento.checked) {
+      return elemento.value;
+    }
+  }
+
 }
 
 
