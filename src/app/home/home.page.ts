e678f444f4845894efc581b99f458af6b83cb4df
hh3 <Ho33@users.noreply.github.com>
2019-11-12 18:25:35 +0100

    some shit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,9 @@
+import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { GeneradorHoteles } from '../core/model/generador-hoteles';
+import { Complemento } from './../core/model/complemento';
 import { Hotel } from './../core/model/hotel';
 import { TipoHabitacion } from './../core/model/tipoHabitacion';
-import { Component } from '@angular/core';
-import { GeneradorHoteles } from '../core/model/generador-hoteles';
-import { Complemento } from '../core/model/complemento';
-import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-home',
@@ -15,11 +15,11 @@ export class HomePage {
 	public miHotel: GeneradorHoteles;
 	private hoteles;
 	private dataTofilter;
-	complemento = {
+	private _complemento = {
 		minibar: false,
-		secador: false,
-	}
-	todo = {
+		secador: false
+	};
+	private _todo = {
 		tipo: '',
 		cama: '',
 		categoria: '',
@@ -30,15 +30,12 @@ export class HomePage {
 		this.dataTofilter = form.value;
 		console.log(this.dataTofilter);
 	}
-	ptionsFn() {
-		console.log(this.todo);
-	}
 	constructor(public router: Router) {
 		this.miHotel = new GeneradorHoteles();
 		this.hoteles = this.miHotel.getHoteles();
 	}
 
-	obtenerPersona() {
+	obetenerHoteles() {
 		let navigationExtras: NavigationExtras = {
 			state: {
 				hoteles: this.hoteles
@@ -55,14 +52,11 @@ export class HomePage {
 	}
 
 	filtrarCategoria() {
-		let retorno = this.hoteles
-			.filter(function(hotel: Hotel) {
-				return hotel.categoria.toString() === this.categoria.toString();
-			})
-			.filter(function(tipoHabitacion: TipoHabitacion) {
-				return tipoHabitacion.complementos.toString() === this.cama.toString();
-			});
-		console.log(this.filtrarCategoria());
+		let that = this._todo;
+		let retorno = this.hoteles.filter(function(hotel: Hotel) {
+			return hotel.categoria === that.categoria.length;
+		});
+		console.log(retorno);
 		return retorno;
 	}
 
@@ -84,11 +78,10 @@ export class HomePage {
 		});
 		return retorno;
 	}
-	/* filtrar(categoria,tipoHabitacion,precio,minibar){
-    let retorno=this.filtrarCategoria(categoria);
-    retorno=this.filtrarTipoHabitacion(tipoHabitacion,retorno.tipoHabitacion);
-    retorno=this.filtrarPrecioHabitacion(precio,retorno.tipoHabitacion);
-    retorno.this.filtrarComplemento(minibar,retorno.tipoHabitacion.complementos);
-    console.log(retorno);
-  }*/
+	get complementos(): boolean {
+		return this._complemento;
+	}
+	get todo(): string {
+		return this._todo;
+	}
 }
